4a64cdcc355619423c86a703a46f6ddf72539eee
Hamza <hamza01961@gmail.com>
2022-05-07 12:15:01 +0500

    [sprint#02] uploadFile backend done

--- controller/DownloadFile.js
+++ controller/DownloadFile.js
@@ -1,8 +1,13 @@
-const fs = require('fs')
 
 async function DownloadFile (req,res){
     const file = JSON.parse(req.query[0])
-    
+    const fileLocation = `uploads/${file.id}`
+    const name = file.name
+    res.download(fileLocation,name,(err)=>{
+        if(err){
+            console.log(err)
+        }
+    })
     // const readFile = fs.createReadStream(`uploads/${file.id}`)
     // console.log('chick')
     // res.setHeader('Content-Disposition',`attachment: filename="${file.name}"`)
